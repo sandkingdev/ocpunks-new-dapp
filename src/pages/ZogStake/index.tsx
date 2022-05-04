ec4bc790a3b9a3e6fde476763dc4b5fce51430c7
sandkingdev <56843906+sandkingdev@users.noreply.github.com>
2022-05-04 06:57:07 -0800

    add buttons

--- src/pages/ZogStake/index.tsx
+++ src/pages/ZogStake/index.tsx
@@ -39,14 +39,14 @@ const ZogStake = () => {
             <div className='row staking-status-body'>
               <div className='col-6 staking-status-body-splite'>
                 <div className='staking-status-body-text'>
-                  <span>Total $ZOG Staked:</span>
+                  <span>Total $ZOG Staked :</span>
                   <br />
                   <span className='staking-status-body-amount'>1000</span>
                 </div>
               </div>
               <div className='col-6'>
                 <div className='staking-status-body-text'>
-                  <span>Reward APR:</span>
+                  <span>Reward APR :</span>
                   <br />
                   <span className='staking-status-body-amount'>200%</span>
                 </div>
@@ -56,11 +56,69 @@ const ZogStake = () => {
           <div className='col-lg-3 col-md-3 col-sm-0 col-xs-0'></div>
         </div>
       </div>
-      <div className='container justify-content-center'>
+      <div className='container justify-content-center ml-3'>
         <div className='row staking-container w-100'>
           <div className='col-12'>
             <div className='row staking-status-body'>
-              <div></div>
+              <div className='col-lg-3 col-md-3 col-sm-12'>
+                <div className='row'>
+                  <div className='col-lg-2 col-md-2 col-sm-12'></div>
+                  <div className='col-lg-10 col-md-10 col-sm-12'>
+                    <div className='staking-card-token'>
+                      <span className='staking-card-token-name'>Staked $ZOG</span>
+                      <span className='staking-card-token-amount'>10000</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='col-lg-3 col-md-3 col-sm-12'>
+                <div className='row'>
+                  <div className='col-lg-2 col-md-2 col-sm-12'></div>
+                  <div className='col-lg-10 col-md-10 col-sm-12'>
+                    <div className='staking-card-token'>
+                      <span className='staking-card-token-name'>Earned $ZOG</span>
+                      <span className='staking-card-token-amount'>10000</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='col-lg-6 col-md-6 col-sm-12' style={{alignSelf: 'center'}}>
+                <div className='row'>
+                  <div className='col-lg-2 col-md-2 col-sm-12'></div>
+                  <div className='col-lg-4 col-md-4 col-sm-12'>
+                    <div className='row'>
+                      <div className='col-12'>
+                        <button className='stake-handle-button'>Stake</button>
+                      </div>
+                    </div>
+                    <div className='row'>
+                      <div className='col-12'>
+                        <button className='stake-handle-button'>UnStake</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='col-lg-4 col-md-4 col-sm-12'>
+                    <div className='row'>
+                      <div className='col-12'>
+                        <button className='stake-handle-button'>Claim</button>
+                      </div>
+                    </div>
+                    <div className='row'>
+                      <div className='col-12'>
+                        <button className='stake-handle-button'>Reinvest</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='col-lg-2 col-md-2 col-sm-12'></div>
+                </div>
+                {/* <div className='row'>
+                  <div className='col-lg-3 col-md-3 col-sm-12'></div>
+                  <div className='col-lg-6 col-md-6 col-sm-12'>
+                    <button className='stake-handle-button'>Stake</button>
+                  </div>
+                  <div className='col-lg-3 col-md-3 col-sm-12'></div>
+                </div> */}
+              </div>
             </div>
           </div>
         </div>
