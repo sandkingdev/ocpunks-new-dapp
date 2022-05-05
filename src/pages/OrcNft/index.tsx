1c82d5885faf8b63adea8a547a5337f9fa08c032
sandkingdev <56843906+sandkingdev@users.noreply.github.com>
2022-05-04 19:37:07 -0800

    add staked component

--- src/pages/OrcNft/index.tsx
+++ src/pages/OrcNft/index.tsx
@@ -9,6 +9,8 @@ import NftCard from 'components/NftCard';
 import {
   useGetAccountInfo,
   useGetNetworkConfig,
+  refreshAccount,
+  sendTransactions,
   useGetPendingTransactions
 } from '@elrondnetwork/dapp-core';
 
@@ -19,6 +21,11 @@ import {
   ProxyProvider,
   Query
 } from '@elrondnetwork/erdjs';
+
+import {
+  convertWeiToEgld,
+  formatNumbers
+} from '../../utils/convert';
 
 import './index.scss';
 
@@ -31,6 +38,9 @@ const OrcNft = () => {
   const [nftDatas, setNftDatas] = useState<any[]>([]);
   const [apy, setApy] = useState(0);
   const [stakedAmount, setStakedAmount] = useState(0);
+
+  const [contractNftDatas, setContractNftDatas] = React.useState<any[]>([]);
+  const [rewards, setRewards] = useState(0);
 
   useEffect(() => {
     axios
@@ -84,6 +94,102 @@ const OrcNft = () => {
       });
   }, [hasPendingTransactions]);
 
+  // staked NFT
+  useEffect(() => {
+
+    const nonces: any = [];
+    // get staked NFTs from sc
+    const query = new Query({
+      address: new Address(contractAddress),
+      func: new ContractFunction('getBalance'),
+      args: [new AddressValue(new Address(address))]
+    });
+
+    const proxy = new ProxyProvider(network.apiAddress, { timeout: TIMEOUT });
+
+    proxy
+      .queryContract(query)
+      .then(({ returnData }) => {
+        const len = returnData.length;
+        if (len > 0) {
+          const data = returnData;
+          for (let i = 0; i < len; i++) {
+            const decoded = Buffer.from(data[i], 'base64').toString('hex');
+            nonces.push(parseInt(decoded, 16));
+          }
+
+          // get NFT datas
+          const nftData: any = [];
+          axios
+            .get(`${gateway}/accounts/${contractAddress}/nfts?from=0&size=2000&collection=${NFT_TOKEN_ID}`)
+            .then((res) => {
+              for (let i = 0; i < res.data.length; i++) {
+                const data = res.data[i];
+                for (let j = 0; j < nonces.length; j++) {
+                  if (data.nonce == nonces[j]) {
+                    nftData.push(data);
+                    break;
+                  }
+                }
+              }
+
+              setContractNftDatas(nftData);
+            });
+        } else {
+          // No Data
+          setContractNftDatas([]);
+        }
+      })
+      .catch((err) => {
+        console.error('Unable to call VM query', err);
+      });
+  }, [hasPendingTransactions]);
+
+  useEffect(() => {
+    const query = new Query({
+      address: new Address(contractAddress),
+      func: new ContractFunction('getCurrentReward'),
+      args: [new AddressValue(new Address(address))]
+    });
+
+    const proxy = new ProxyProvider(network.apiAddress, { timeout: TIMEOUT });
+
+    proxy
+      .queryContract(query)
+      .then(({ returnData }) => {
+        const [encoded] = returnData;
+        if (encoded == undefined || encoded == '') {
+          setRewards(0);
+        } else {
+          const decoded = Buffer.from(encoded, 'base64').toString('hex');
+          const value = convertWeiToEgld(parseInt(decoded, 16), REWARD_TOKEN_DECIMAL);
+          setRewards(value);
+        }
+      })
+      .catch((err) => {
+        console.error('Unable to call VM query', err);
+      });
+  }, [hasPendingTransactions]);
+
+  const handleClaim = async () => {
+
+    const reinvestTransaction = {
+      data: 'claim',
+      receiver: contractAddress
+    };
+
+    await refreshAccount();
+    await sendTransactions({
+      transactions: reinvestTransaction,
+      transactionsDisplayInfo: {
+        processingMessage: 'Processing Claim transaction',
+        errorMessage: 'An error has occured during Claim',
+        successMessage: 'Claim transaction successful'
+      },
+      redirectAfterSign: false
+    });
+  };
+
   return (
     <div className='container'>
       <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
@@ -106,7 +212,19 @@ const OrcNft = () => {
         </Tab>
         <Tab eventKey={2} title="Staked Orcpunks" className='nft-tab'>
           <div className='nft-tab'>
-            Tab 2 content
+            <div className='row text-center'>
+              <div className='col-lg-6 col-md-6 col-sm-12 rewards-amount'>Reward $ZOG : {formatNumbers(rewards)}</div>
+              <div className='col-lg-6 col-md-6 col-sm-12'>
+                <button className='btn btn-primary btn-claim' onClick={handleClaim}>Claim</button>
+              </div>
+            </div>
+            <div className='row mt-5'>
+              {contractNftDatas.map((item, key) => {
+                return <div className='col-lg-3 col-md-3 col-sm-12' key={key}>
+                  <NftCard item={item} type={false} />
+                </div>;
+              })}
+            </div>
           </div>
         </Tab>
       </Tabs>
