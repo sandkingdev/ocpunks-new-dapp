572baa2306ec86ddd260f6158058474c9080e58f
sandkingdev <56843906+sandkingdev@users.noreply.github.com>
2022-05-06 04:36:54 -0800

    deploy mainnet

--- src/config.tsx
+++ src/config.tsx
@@ -1,27 +1,27 @@
 export const dAppName = 'NFT Staking';
 
 // ZOG staking
-export const ZOG_STAKING_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgq0mhxq4tsmssljjzaav73hdjas043ys575zvs79z9pe';
-export const STAKE_TOKEN_ID = 'ZOG-481946';
+export const ZOG_STAKING_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqan35jlf5tq8a4m7hjjxn3twqv4dscfgd7lnqqdcva7';
+export const STAKE_TOKEN_ID = 'ZOG-c66239';
 
 // OrcpunksNFT staking
 export const ORC_NFT_STAKING_CONTRACT_ADDRESS =
-  'erd1qqqqqqqqqqqqqpgqzcu8k5dawkv2jewa74wwnap7x283fmy75zvsa6azf3';
-export const ORC_NFT_TOKEN_ID = 'ORC-0427ce';
+  'erd1qqqqqqqqqqqqqpgqlr8jdck0lyelwu66wgq2ttc9fs7gqw5f7lnq90fv53';
+export const ORC_NFT_TOKEN_ID = 'ORC-ef544d';
 
 // EasterNFT staking
 export const EASTER_NFT_STAKING_CONTRACT_ADDRESS =
-  'erd1qqqqqqqqqqqqqpgqrler5y48pg9s6qkunxyq0azth85lvn3v5zvsr9404r';
-export const EASTER_NFT_TOKEN_ID = 'EAS-da9026';
+  'erd1qqqqqqqqqqqqqpgq77rwyqem6yd7hgtnlhqwqff9k40gtjsn7lnqnp0f3u';
+export const EASTER_NFT_TOKEN_ID = 'ESTO-662d61';
 
 export const SWAP_CONTRACT_ADDRESS =
-  'erd1qqqqqqqqqqqqqpgqj6csja68fagx5ggsl3kck39fuj3gqpsl5zvsay7nng';
+  'erd1qqqqqqqqqqqqqpgqkq9wkkzykg9qxt5y0uncrf7lfhx5ta2y7lnqcukq0l';
 export const NFT_PRICE = 50000;
 
 export const REWARD_TOKEN_DECIMAL = 6;
 export const TIMEOUT = 6000;
 
-// export const ENVIRONMENT = 'mainnet';
-// export const gateway = 'https://api.elrond.com';
-export const ENVIRONMENT = 'devnet';
-export const GATEWAY = 'https://devnet-api.elrond.com';
+export const ENVIRONMENT = 'mainnet';
+export const GATEWAY = 'https://api.elrond.com';
+// export const ENVIRONMENT = 'devnet';
+// export const GATEWAY = 'https://devnet-api.elrond.com';
