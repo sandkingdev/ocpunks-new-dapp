6492ea37e6ab8a6fef0433615f8020dcea01e337
sandkingdev <56843906+sandkingdev@users.noreply.github.com>
2022-05-10 17:17:55 -0800

    add flip type button

--- src/pages/Coinflip/index.tsx
+++ src/pages/Coinflip/index.tsx
@@ -53,16 +53,47 @@ const Coinflip = () => {
   const { network } = useGetNetworkConfig();
   const isLoggedIn = Boolean(address);
 
+  const [flipType, setFlipType] = useState(0);
+  const [headStyle, setHeadStyle] = useState('choose-button select-type-button');
+  const [tailStyle, setTailStyle] = useState('choose-button ');
+
+  const handleFlipType = (type: any) => {
+    if (type === 0) {
+      // head
+      setHeadStyle('choose-button select-type-button');
+      setTailStyle('choose-button');
+      setFlipType(0);
+    } else if (type === 1) {
+      // tail
+      setHeadStyle('choose-button');
+      setTailStyle('choose-button select-type-button');
+      setFlipType(1);
+    }
+  };
+
 
   return (
     <div className='container'>
-      <div className='row justify-content-center'>
+      {/* <div className='row justify-content-center'>
         <video loop autoPlay>
           <source
             src='../../assets/movie/animation.mp4'
             type="video/mp4"
           />
         </video>
+      </div> */}
+      <div className='row justify-content-center mt-3'>
+        <p className='flip-type'>I choose</p>
+      </div>
+      <div className='row'>
+        <div className='col-lg-3 col-md-3 col-sm-12 col-xs-12'></div>
+        <div className='col-lg-3 col-md-3 col-sm-12 col-xs-12 head-container'>
+          <button className={headStyle} onClick={() => handleFlipType(0)}>HEADS</button>
+        </div>
+        <div className='col-lg-3 col-md-3 col-sm-12 col-xs-12 tail-container'>
+          <button className={tailStyle} onClick={() => handleFlipType(1)}>TAILS</button>
+        </div>
+        <div className='col-lg-3 col-md-3 col-sm-12 col-xs-12'></div>
       </div>
     </div>
   );
