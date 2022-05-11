a4d7b89f01c28351feeb5af02b757847141636e1
sandkingdev <56843906+sandkingdev@users.noreply.github.com>
2022-05-10 18:58:53 -0800

    add contract

--- src/pages/Coinflip/index.tsx
+++ src/pages/Coinflip/index.tsx
@@ -1,52 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import BigNumber from 'bignumber.js/bignumber.js';
-import { routeNames } from 'routes';
-
-import { ZOG_STAKING_CONTRACT_ADDRESS, GATEWAY, TIMEOUT, STAKE_TOKEN_ID, REWARD_TOKEN_DECIMAL } from 'config';
+import { Container, Row, Col, Dropdown, Form, Table } from 'react-bootstrap';
 
 import {
-  useGetAccountInfo,
-  useGetNetworkConfig,
   refreshAccount,
   sendTransactions,
-  useGetPendingTransactions
+  useGetAccountInfo,
+  useGetNetworkConfig,
+  useGetPendingTransactions,
 } from '@elrondnetwork/dapp-core';
-
 import {
   Address,
-  AddressValue,
-  ContractFunction,
+  AbiRegistry,
+  SmartContractAbi,
+  SmartContract,
   ProxyProvider,
-  Query
-} from '@elrondnetwork/erdjs';
-
-import {
-  GasLimit,
+  TypedValue,
   BytesValue,
   BigUIntValue,
-  Egld,
-  TypedValue,
   ArgSerializer,
-  TransactionPayload,
-  Transaction,
-  Balance,
-} from '@elrondnetwork/erdjs/out';
+  GasLimit,
+  OptionalValue,
+  U32Value,
+} from '@elrondnetwork/erdjs';
 
 import {
   convertWeiToEgld,
   formatNumbers
 } from '../../utils/convert';
 
-import StakingModal from 'components/Modal';
-
 // import {ZogIcon} from '../../assets/movie/animation.mp4';
 import './index.scss';
 
 const Coinflip = () => {
-
-  const navigate = useNavigate();
 
   const { address } = useGetAccountInfo();
   const { hasPendingTransactions } = useGetPendingTransactions();
