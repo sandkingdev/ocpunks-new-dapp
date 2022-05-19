a2de83ade8077cfcafa2c4229d5e9931783e0bb9
sandkingdev <56843906+sandkingdev@users.noreply.github.com>
2022-05-19 02:25:14 -0800

    change text

--- src/pages/Breeding/index.tsx
+++ src/pages/Breeding/index.tsx
@@ -32,13 +32,7 @@ const Breeding = () => {
       <Container className='custom-presale-container'>
         <Row>
           <Col lg={12} md={12} sm={12}>
-            <h1 className='custom-nft-mint-text-header'>Orcpunks NFTs have many goals, the first one is to reward holders.<br />Staking is now live!!!</h1>
-          </Col>
-          <Col lg={12} md={12} sm={12}>
-            <h5 className='custom-nft-mint-text-description'>ZorgCoin is a unique multipurpose token, that will have a broad usage in the Orcverse, from buying NFT, merch, games, as a ingame currency, 3D models for board games. MINT AND EARN</h5>
-          </Col>
-          <Col lg={12} md={12} sm={12} className='custom-nft-mint-button-container'>
-            <Button className='custom-nft-mint-buttons' onClick={handleMint}>Stake NOW</Button>
+            <h1 className='custom-nft-mint-text-header'>COMING SOON!!!</h1>
           </Col>
         </Row>
       </Container>
