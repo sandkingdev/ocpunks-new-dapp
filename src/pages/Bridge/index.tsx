4eb7c4d7139a56783ade4bd29b50cbfa1cf0e239
sandkingdev <56843906+sandkingdev@users.noreply.github.com>
2022-07-26 15:18:50 +0200

    frame

--- src/pages/Bridge/index.tsx
+++ src/pages/Bridge/index.tsx
@@ -14,7 +14,7 @@ const Bridge = () => {
     <Container className='custom-bridge-widget'>
       <Row>
         <Col lg={12} md={12} sm={12}>
-          <iframe src='https://widget.xp.network/?widget=true&background=&panelBackground=e6e1e1&modalBackground=efecec&color=14161a&fontSize=16&btnColor=ffffff&btnBackground=395FEB&btnRadius=9&fontFamily=Roboto&chains=BSC-Elrond-Polygon&from=&to=&cardBackground=1e222d&cardBackgroundBot=1e222d&cardColor=ffffff&cardRadius=25&secondaryColor=0c0d0d&accentColor=3e64ed&borderColor=988b8b&iconColor=3e64ed&tooltipBg=1D212A&tooltipColor=ffffff&wallets=MetaMask-WalletConnect-TrustWallet-MyAlgo-AlgoSigner-TronLink-Maiar-Beacon-TempleWallet-MaiarExtension&bridgeState=undefined&showLink=true&affiliationFees=1' frameBorder='0' width="100%" height="600px"></iframe>
+          <div style='width:100%;height:800px'><iframe src='https://widget.xp.network/?widget=true&background=&panelBackground=e6e1e1&modalBackground=efecec&color=14161a&fontSize=16&btnColor=ffffff&btnBackground=395FEB&btnRadius=9&fontFamily=Roboto&chains=BSC-Elrond-Polygon&from=&to=&cardBackground=1e222d&cardBackgroundBot=1e222d&cardColor=ffffff&cardRadius=25&secondaryColor=0c0d0d&accentColor=3e64ed&borderColor=988b8b&iconColor=3e64ed&tooltipBg=1D212A&tooltipColor=ffffff&wallets=MetaMask-WalletConnect-TrustWallet-Maiar-MaiarExtension&bridgeState=undefined&showLink=true&affiliationFees=1.1' frameborder='0' width="100%" height="100%" id="xpnetWidget"></iframe></div>
         </Col>
       </Row>
     </Container>
