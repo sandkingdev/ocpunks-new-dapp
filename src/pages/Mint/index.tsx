7a8f8a81e246df983f350d2ddcd3ecbaf4aeda57
sandkingdev <56843906+sandkingdev@users.noreply.github.com>
2022-08-18 10:39:37 +0200

    mint page

---
+++ src/pages/Mint/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  Container,
+  Row,
+  Col
+} from 'react-bootstrap';
+
+import './index.scss';
+
+const Mint = () => {
+
+
+  return (
+    <Container className='mint-container'>
+      <Row>
+        <Col lg={12} md={12} sm={12}>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Mint;
